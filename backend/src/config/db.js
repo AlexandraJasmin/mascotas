53b9918c5120c337e22b5c75ca8295d1b8855b2f
Alexandra <alexapadilla14059@gmail.com>
2026-05-04 10:08:37 -0600

    Usar DATABASE_URL para Render

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -5,10 +5,14 @@ dotenv.config();
 
 const { Pool } = pg;
 
-export const pool = new Pool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT
-});
+export const pool = process.env.DATABASE_URL
+  ? new Pool({
+      connectionString: process.env.DATABASE_URL
+    })
+  : new Pool({
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      port: process.env.DB_PORT
+    });
